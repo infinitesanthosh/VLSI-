bce20ca03a324e18a4297f57f843012ef4d26ac4
infinitesanthosh <infinitesanthosh1729@gmail.com>
2025-09-15 16:23:50 +0530

    Update and.v

--- verilog/assignment1/and.v
+++ verilog/assignment1/and.v
@@ -1,1 +1,8 @@
-Write a Verilog module using a continuous assignment to implement out = a & b.
+//Write a Verilog module using a continuous assignment to implement out = a & b.
+module and_continuous (
+    input  wire a,
+    input  wire b,
+    output wire out
+);
+    assign out = a & b;
+endmodule
